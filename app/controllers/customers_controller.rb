6bbb9cbc4d900e53dc5ecbdc5ef3fdd3133c08cf
your name hereJoJo <Jojocohen57@gmail.com>
2020-10-05 16:54:42 -0400

    Adds customer controller

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,29 @@
+class CustomersController < ApplicationController
+
+    before_action :redirect_if_logged_in
+  
+    layout 'application'
+  
+    def new
+        @customer = Customer.new
+    end
+  
+    def create
+      @customer = Customer.new(customer_params)
+      if @customer.save
+        session[:customer_id] = @customer.id
+        redirect_to artists_path
+        # redirect_to customer_path(@customer)
+      else
+        # @errors = @customer.errors.full_messages
+        render :new
+      end
+    end
+  
+    private
+  
+    def customer_params
+      params.require(:customer).permit(:username, :password, :password_confirmation)
+    end
+  
+  end
